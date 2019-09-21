43b677dece66c4c19ee85e733b19061be111b7ce
lishujun5 <624297561@qq.com>
2019-09-21 10:22:38 +0800

    [CHG]学习cout输出符

--- 10/10_6.cpp
+++ 10/10_6.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <iomanip>
 using namespace std;
 int main(int argc , char *argv[])
 {
     vector<int> obj;
+    int a = 2;
     fill_n(back_inserter(obj),10,0);
     for(auto e : obj)
     {
         cout<<e<<endl;
     }
+    cout<<obj.size()<<endl;
+    cout<<setfill('x')<<setiosflags(ios::left)<<setw(2)<<a<<endl;
     return 0;
 }
